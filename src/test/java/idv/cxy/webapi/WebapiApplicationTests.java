7dc1b1593d3cf4229c1df1a791745988de62cf36
CXY <idea0816@gmail.com>
2022-09-05 15:37:00 +0700

    初始化 webapi

---
+++ src/test/java/idv/cxy/webapi/WebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.cxy.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
